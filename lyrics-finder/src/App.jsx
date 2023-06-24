a3ba56e3e8a4ee94e3675577dc9b5765301c9515
Joseph Suberu <opensesame4me@gmail.com>
2023-06-24 23:45:24 +0100

    created a react project with with vite and tailwind css

---
+++ lyrics-finder/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/layouts/Navbar.jsx';
+
+
+
+function App() {
+ 
+
+  return (
+    <div className='py-[20px] min-h-[100vh] bg-gradient-to-r from-[rgb(2,0,36)] via-[rgba(9,9,121)] to-[rgba(0,212,255)]'>
+      
+      
+        <Navbar />
+        
+      
+    </div>
+  )
+}
+
+export default App
---
+++ lyrics-finder/src/components/layouts/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Navbar = ()=>{
+
+    return(
+        <div className='container border text-center'>
+            <h2 className='text-white text-xl'>Lyrics Finder</h2>
+            <h4>search for your favourite song's lyrics</h4>
+        </div>
+    )
+}
+
+export default Navbar;
